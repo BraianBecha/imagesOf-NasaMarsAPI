b7eeb0fa31aa09a88c91944ca03d39f43544b15d
BraianBecha <braian.becha.hace@gmail.com>
2024-04-14 15:32:18 -0300

    agrego_Version_Vue

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<script setup>
+import FormInitVue from './components/FormComponent.vue';
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import TopBar from './components/TopBar.vue';
+import FormComponent from './components/FormComponent.vue';
+import CardPhoto from './components/CardPhoto.vue';
+
+
+
+</script>
+
+<script>
+export default {
+  components: { CardPhoto },
+  data() {
+    return {
+      message: 'This is some text',
+      linkServicio : "https://api.nasa.gov/mars-photos/api/v1/rovers/",
+      linkConsulta : "",
+      arrayPresentacionPhotos : [],      
+      presentacionFoto : {id:0 , imgSrc:"" , earth_date:"" , rover:"" , camera :"" }
+    };
+  },
+  methods:{
+    receiveValues(x, y, z)
+    {
+    console.log(`los valores escuchados en appVue son ${x} , ${y} , ${z}`)
+    let elLink=this.crearLinkConsulta(x,y,z)
+    console.log(`el link creado es ${this.linkConsulta}`)
+
+  },
+    
+    crearLinkConsulta(x,y,z){    
+    this.linkConsulta=this.linkServicio.concat(`${x}/photos?sol=${y}&api_key=${z}`)
+    console.log(`en crearLinkConsulta() link es ${this.linkConsulta}`)
+    this.buscarFotos(this.linkConsulta)
+    
+    
+},
+ buscarFotos(x){
+  this.arrayPresentacionPhotos=[];
+  fetch(x)
+.then(z=>z.json())
+        .then((y)=> {console.log(`y es ${y} la cantidad de fotos es ${y.photos.length}`)  
+        y.photos.forEach(element => { 
+          let unaPresentacion=Object.create(this.presentacionFoto)
+                        unaPresentacion.id=element.id
+                        unaPresentacion.imgSrc=element.img_src
+                        unaPresentacion.earth_date=element.earth_date
+                        unaPresentacion.rover=element.rover.name
+                        unaPresentacion.camera=element.camera.name                        
+                    this.arrayPresentacionPhotos.push(unaPresentacion)})
+                   // imprimirPhotos(arrayPresentacionPhotos) 
+        })
+        
+}
+
+}
+};
+
+</script>
+
+<template> 
+
+<top-bar />
+ <main>
+  <form-component @send-values="receiveValues" />
+
+  <div class="contenedorDeFotos">
+ <card-photo v-for="x in arrayPresentacionPhotos" :key="x.id"
+ :id-photo="x.id"
+ :img-src="x.imgSrc"
+ :earth-date="x.earth_date"
+ :rover-name="x.rover"
+ :camera-name="x.camera" />   
+  </div>
+ 
+  </main>
+</template>
+
+<style scoped>
+
+.contenedorDeFotos{
+    display : flex ;
+    flex-direction : column ;
+    justify-content: center;
+    
+  place-items: center;
+    justify-content:space-between;
+    max-width: 95vw;
+    margin: 20px;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+}
+</style>
---
+++ src/components/CardPhoto.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="tarjetaFoto">
+   <img :src="imgSrc">
+   <div class="listaDescriptiva">
+   <ol>
+       <li>Id : {{ idPhoto }} </li>
+       <li>Earth date: {{ earthDate }} </li>
+       <li>Rover name: {{ roverName }}</li>
+       <li>Camera: {{ cameraName }}   </li>
+   </ol>
+   </div>          
+</div>
+</template>
+
+<script>
+  export default {
+    props: ['idPhoto','imgSrc', 'earthDate', 'roverName', 'cameraName']
+  }
+</script>
+
+<style>
+
+
+
+
+
+.tarjetaFoto img {
+  object-fit: contain;
+  max-width: 100%;
+    max-height: 90vh;
+}
+.tarjetaFoto{
+    max-width: 1200px;    
+    border-width : 3px;
+    border: 2px solid var(--gris);
+    border-radius : 20px;
+    margin: 30px;
+    padding: 30px;
+
+}
+.listaDescriptiva{
+    display: flex;
+    justify-content: center;
+   max-width: 700px;
+   left: 200px;
+    
+}
+</style>
