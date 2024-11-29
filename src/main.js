71a68b52d970d4e35225b8718664abf87d2362d9
BraianBecha <braian.becha.hace@gmail.com>
2024-11-29 19:28:01 -0300

    cambio base en viteconfig

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import App from './App.vue'
-import { createApp } from 'vue'
 
+import { createApp } from '../node_modules/.vue-global-types/vue_3.4_false'
+import App from './App.vue'
 createApp(App).mount('#app')
