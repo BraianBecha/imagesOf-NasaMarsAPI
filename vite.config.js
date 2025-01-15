07881d2f60615cb847b326d60994ec0326028437
BraianBecha <braian.becha.hace@gmail.com>
2025-01-15 14:58:00 -0300

    agrego index en la ruta anterior

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
    
   plugins: [vue()],
-  base: 'https://BraianBecha/imagesOf-NasaMarsAPI'
+  base: 'https://BraianBecha/imagesOf-NasaMarsAPI/index.html'
   ,
   resolve: {
     alias: {
